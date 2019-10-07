de5c28947321ba711aa338d5abfa63cda4d8b8e4
VincentDaSilvaPinto <vdasilvapin@ensc.fr>
2019-10-07 11:54:52 +0200

    modification environnement

--- Projet-SMA/Assets/Script/Agent.cs
+++ Projet-SMA/Assets/Script/Agent.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Agent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent _agent;
+    private Transform _moveTarget;
+    private void Start()
     {
-        
+        _agent = GetComponent(typeof(NavMeshAgent)) as NavMeshAgent;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                var clickPosition = hit.transform.position;
+                _agent.SetDestination(clickPosition);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
