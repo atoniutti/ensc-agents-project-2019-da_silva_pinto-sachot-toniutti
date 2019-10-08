3588c0c56883fe4f830d6fc273e64ff8136bb365
VincentDaSilvaPinto <vdasilvapin@ensc.fr>
2019-10-08 19:49:00 +0200

    Ajout de la classe pour le champ de vision

--- Projet-SMA/Assets/Scripts/Agent.cs
+++ Projet-SMA/Assets/Scripts/Agent.cs
@@ -6,13 +6,19 @@ using UnityEngine.AI;
 public class Agent : MonoBehaviour
 {
     public NavMeshAgent _agent;
+    public FieldOfView _fieldOfView;
+    private Vector3 targetPileEnergy ;
     private Transform _moveTarget;
+    private Animator animator;
     private void Start()
     {
         _agent = GetComponent(typeof(NavMeshAgent)) as NavMeshAgent;
+        animator = GetComponent<Animator>();
+        targetPileEnergy = new Vector3(-0.98f, 0f, -3.65f);
     }
     private void Update()
     {
+        //inutile 
         if (Input.GetMouseButton(0))
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,6 +29,21 @@ public class Agent : MonoBehaviour
                 _agent.SetDestination(clickPosition);
             }
         }
+       
+            
+        //detection dans le champs de vision
+        if (_fieldOfView.energyFront == true)
+        {
+            animator.SetBool("walk", true);
+            _agent.SetDestination(targetPileEnergy);
+            if (transform.position.x == targetPileEnergy.x && transform.position.z == targetPileEnergy.z)
+                {
+                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    animator.SetBool("walk", false);
+            }
+        }
+       
     }
+    
 
 }
---
+++ Projet-SMA/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public bool energyFront= false;
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.name == "EnergyCoil")
+        {
+            Debug.Log("energy");
+            energyFront = true;
+        }
+    }
+}
