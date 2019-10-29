c83e4cff0b3938ff7d3c79a84ba7ce313b3b8c04
Toniutti Andrea <36880234+atoniutti@users.noreply.github.com>
2019-10-29 14:09:56 +0100

    Suppresion Prefabs inutiles

--- Projet-SMA/Assets/Scripts/Managers/AgentManager.cs
+++ Projet-SMA/Assets/Scripts/Managers/AgentManager.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimulationManager : MonoBehaviour
+public class AgentManager : MonoBehaviour
 {
     public GameObject agent;                // The agent prefab to be spawned
-    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from
+    
+    Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from
 
     // Start is called before the first frame update
     void Start()
     {
-        // Find a random index between zero and one less than the number of spawn points.
+        // Find a random index between zero and one less than the number of spawn points
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
         Spawn(spawnPointIndex);
