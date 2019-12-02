28d3c7e1587d8141126c0718c22a6397c011501c
VincentDaSilvaPinto <vdasilvapin@ensc.fr>
2019-12-02 15:41:04 +0100

    Update Agent.cs

--- Projet-SMA/Assets/Scripts/Agents/Agent.cs
+++ Projet-SMA/Assets/Scripts/Agents/Agent.cs
@@ -481,12 +481,12 @@ public class Agent : MonoBehaviour
     public AgentStates MakeAChoiceState(float percentOfEnergy, float percentOfToxic)
     {
         float proba = Random.Range(0f, 1f);
-        if (percentOfEnergy >= 0 && percentOfEnergy <= 99 && percentOfToxic <= 20)
+        if (percentOfEnergy >= 0 && percentOfEnergy <= 99 && percentOfToxic <= 30)
         {
             AgentStates state = AgentStates.FindingEnergy;
             return state;
         }
-        if (percentOfEnergy >= 20 && percentOfEnergy <= 99 && percentOfToxic > 20 && percentOfToxic < 70)
+        if (percentOfEnergy >= 0 && percentOfEnergy <= 99 && percentOfToxic > 30 && percentOfToxic < 70)
         {
             if (proba >= 0.6f)
             {
