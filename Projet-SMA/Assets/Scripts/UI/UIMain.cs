3238ae7870a6cf9c18fa66a66d3761035367a9c4
Toniutti Andrea <36880234+atoniutti@users.noreply.github.com>
2019-12-10 17:39:50 +0100

    First Look of PauseMenu

--- Projet-SMA/Assets/Scripts/UI/UIMain.cs
+++ Projet-SMA/Assets/Scripts/UI/UIMain.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMain : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class UIMain : MonoBehaviour
     public ProgressBar _energyProgressBar;
     public ProgressBar _wasteProgressBar;
 
+    // Slider
+    public Slider _energySlider;
+    public Slider _wasteSlider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +26,12 @@ public class UIMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Update Percent Of Energy & Toxic Piles
+        // Update Percent Of Energy & Waste Piles
         _energyProgressBar.UpdateBar(_energyLevel._currentPercent);
         _wasteProgressBar.UpdateBar(_wasteLevel._currentPercent);
+
+        // Update Slider Of Energy & Waste Piles
+        _energySlider.value = _energyLevel._currentPercent;
+        _wasteSlider.value = _wasteLevel._currentPercent;
     }
 }
