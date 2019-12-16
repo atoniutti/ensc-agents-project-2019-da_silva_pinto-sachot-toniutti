278e25f0209def52d681a9641d5746e1fcad311d
Toniutti Andrea <36880234+atoniutti@users.noreply.github.com>
2019-12-16 16:02:36 +0100

    Chrono

---
+++ Projet-SMA/Assets/Scripts/UI/TimePanel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePanel : MonoBehaviour
+{
+    public Text _time;
+    public PauseCanvas _pausePanel;
+
+    Stopwatch _stopWatch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _stopWatch = new Stopwatch();
+        _stopWatch.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _stopWatch.Stop();
+
+        // Get the elapsed time as a TimeSpan value
+        TimeSpan ts = _stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        ts.Hours, ts.Minutes, ts.Seconds,
+        ts.Milliseconds / 10);
+        _time.text = elapsedTime;
+
+        if (!_pausePanel.GetGameIsPaused())
+        {
+            _stopWatch.Start();
+        }
+    }
+}
